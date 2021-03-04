3a1a15f7315b375acb6b3f67c44b752bcf781416
unknown <kuldipkumarsah112@gmail.com>
2021-03-05 01:02:49 +0530

    day-2

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.get('/home', function (req, res) {
+
+    const jsonObject={
+        firstName:"kuldip",
+        lastName:"sah"
+    }
+    res.send(jsonObject)
+  });
+  
+  app.post('/home', function (req, res) {
+      console.log(req.body);
+    // res.send('This is a POST request')
+    res.send(req.body)
+  })
 
 app.listen(3000, 'localhost', function run() {
     console.log('App is running 3000 port');
